24fcf91dc89ff369807197ea9d03c2b5fbbff5ae
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-04-01 21:02:38 +0300

    Add basic starter

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net;
 
 namespace Server
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var server = new Server(IPAddress.Any, 25565);
+            server.UserConnected += OnUserConnected;
+            server.UserDisconnected += OnUserDisconnected;
+            server.Start();
+
+            Console.WriteLine($"Server started ad {IPAddress.Any}:25565");
+            Console.ReadLine();
+        }
+
+        private static void OnUserConnected(object sender, GameEventArgs args)
+        {
+            Console.WriteLine($"Connected {args.Username}");
+        }
+
+        private static void OnUserDisconnected(object sender, GameEventArgs args)
+        {
+            Console.WriteLine($"Disconnected: {args.Username}");
         }
     }
 }
